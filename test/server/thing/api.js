770df969ecab37ce9bce2652190f0eb19bb98e94
wjmao88 <accounts@hackreactor.com>
2014-04-03 19:24:30 -0700

    refactored server generation

--- test/server/thing/api.js
+++ test/server/thing/api.js
@@ -2,28 +2,71 @@
 
 var should = require('should'),
     app = require('../../../server'),
-    request = require('supertest');
+    request = require('supertest'),
+    mongoose = require('mongoose'),
+    User = mongoose.model('User');
 
-var resources = [
-  'user',
-  'server'
-]
+describe('users api', function() {
+  var userId;
+  var userJson = {
+    provider: 'local',
+    name: 'Fake User 2',
+    email: 'test@test.com',
+    password: 'password'
+  };
 
-for (var r=0, length=resources.length; r<length; r++){
-
-  describe('GET /api/' + resources[r], function() {
-
-    it('should respond with JSON array', function(done) {
+  it('should respond to post', function(done) {
+    User.remove().exec().then(function(){
       request(app)
-        .get('/api/awesomeThings')
+        .post('/api/users')
+        .send(userJson)
         .expect(200)
-        .expect('Content-Type', /json/)
         .end(function(err, res) {
           if (err) return done(err);
-          res.body.should.be.instanceof(Array);
           done();
         });
     });
   });
 
-}
+  it('should respond to get using id', function(done) {
+    User.remove().exec();
+    request(app)
+      .post('/api/users')
+      .send(userJson)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        User.find({}).exec()
+          .then(function(err, users){
+            console.log(users);
+            request(app)
+              .get('/api/users/'+userJson.username)
+              .expect(200)
+              .expect('Content-Type', 'application/json; charset=utf-8')
+              .end(function(err, res) {
+                if (err) return done(err);
+                done();
+              });
+          });
+      });
+  });
+});
+
+/*
+  app.post('/api/users', users.create); +
+  app.put('/api/users', users.changePassword);
+  app.get('/api/users/me', users.me);
+  app.get('/api/users/:id', users.show);
+
+  app.post('/api/session', session.login);
+  app.del('/api/session', session.logout);
+
+  app.get('/api/servers', servers.list);
+  app.post('/api/servers', servers.create);
+  app.get('/api/servers/:name', servers.show);
+  app.post('/api/servers/:name', servers.update);
+  app.del('/api/servers/:name', servers.remove);
+
+  app.get('api/servers/:name/start', servers.start);
+  app.del('api/servers/:name/stop', servers.stop);
+ */
