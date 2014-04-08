8d09e456d3207d1b8b09104c24e56622e5d8c706
agchou <aarongchou@gmail.com>
2014-04-08 15:07:31 -0700

    fixes bug in dashboard.js

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -1,24 +1,23 @@
 'use strict';
 
-angular.module('hackathonApp');
+angular.module('hackathonApp')
   .factory('ServerService', function($http){
     var service = {
-      startServer: function(server){
-        return $http.post('api/servers/'+server._id+'/start');
+      startServer: function (server) {
+        return $http.post('api/servers/' + server._id + '/start');
       },
-      stopServer: function(server){
-        return $http.post('api/servers/'+server._id+'/stop');
+      stopServer: function (server) {
+        return $http.post('api/servers/' + server._id + '/stop');
       },
-      deleteServer: function(server){
-        var confirmDelete = confirm('Are you sure you wish to delete '+server.name+'?');
+      deleteServer: function (server) {
+        var confirmDelete = confirm('Are you sure you wish to delete ' + server.name + '?');
         if (confirmDelete) {
-          return $http.delete('api/servers/'+server._id);
+          return $http.delete('api/servers/' + server._id);
         }
       },
     };
     return service;
-  });
-
+  })
   .controller('DashboardCtrl', function($scope, $http, $route, ServerService) {
 
     $http.get('/api/servers').success(function(servers) {
@@ -26,9 +25,9 @@ angular.module('hackathonApp');
 
       $scope.servers.forEach(function (server) {
         Object.keys(server.resources).forEach(function (resource) {
-          console.log("server:",server);
+          console.log('server:', server);
           //server = {name:"",port="",url="",resources={}}
-          console.log("server.resourses:", server.resources);
+          console.log('server.resourses:', server.resources);
           //server.resources = {users="", messages="", rooms=""}
           //console.log(server.resources[resource]);
           server.resources[resource].description = '/' + resource + ': ';
@@ -39,27 +38,27 @@ angular.module('hackathonApp');
     // ServerService
     $scope.defaults = ServerService.defaults;
     $scope.deleteServer = function(server){
-      ServerService.deleteServer(server).success(function() {
+      ServerService.deleteServer(server).success(function () {
         // remove server from client view
-        $scope.servers.splice($scope.servers.indexOf(server),1);
-        console.log("Server deleted");
+        $scope.servers.splice($scope.servers.indexOf(server), 1);
+        console.log('Server deleted');
         // reload server list
         $route.reload();
       });
-    }
+    };
     $scope.startServer = function(server){
       if (server.status.running) {
         ServerService.stopServer(server).success(function() {
           // refactor to use ng-show and ng-hide, look into loading gif, with on event listening and response end
-          console.log("Server stopped")
+          console.log('Server stopped');
           //$route.reload();
-        });    
+        });
       } else {
         ServerService.startServer(server).success(function() {
           // refactor to use ng-show and ng-hide, look into loading gif, with on event listening and response end
-          console.log("Server started")
+          console.log('Server started');
           //$route.reload();
         });
       }
-    }  
+    };
   });
