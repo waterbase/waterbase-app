69c1eae2fd9610ec1a115c337e9f4a78de574d05
RitzyTunas <7riste@gmail.com>
2014-04-08 13:13:34 -0700

    initila dashboard view

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -1,7 +1,25 @@
 'use strict';
 
 angular.module('hackathonApp')
-  .controller('DashboardCtrl', function ($scope, $http) {
+  .factory('ServerService', function ($http) {
+    var service = {
+      startServer: function (server){
+        return $http.post('api/servers/'+server._id+'/start');
+      },
+      stopServer: function (server){
+        return $http.post('api/servers/'+server._id+'/stop');
+      },
+      deleteServer: function (server){
+        var confirmDelete = confirm('Are you sure you wish to delete '+server.name+'?');
+        if (confirmDelete) {
+          return $http.delete('api/servers/'+server.id);
+        }
+      }
+    }
+    return service;
+  })
+
+  .controller('DashboardCtrl', function ($scope, $http, $route, ServerService) {
     $http.get('/api/servers').success(function(servers) {
       $scope.servers = servers;
 
@@ -13,4 +31,24 @@ angular.module('hackathonApp')
         });
       });
     });
+    
+    $scope.startServer = function (server) {
+      if (server.status.running) {
+        ServerService.stopServer(server).success(function() {
+          console.log('Server stopped');
+        })
+      } else {
+        ServerService.startServer(server).success(function() {
+          console.log('Server started');
+        })
+      }
+    };
+
+    $scope.deleteServer = function (server) {
+      ServerService.deleteServer(server).success(function() {
+        $scope.servers.splice($scope.servers.indexOf(server), 1);
+        console.log('Server deleted');
+        $route.reload();
+      })
+    };
   });
