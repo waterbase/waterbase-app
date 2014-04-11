ecef3ac09ab476b5e4e1eb81ee62dbad9f69b0ae
Charles Tai <cwtai86@gmail.com>
2014-04-10 23:35:05 -0700

    Able to edit documents

--- app/scripts/controllers/manager.js
+++ app/scripts/controllers/manager.js
@@ -1,31 +1,14 @@
 'use strict';
 
 
-    // $http.post('/api/database/super/collection/users/id',
-    // {documents: { name: 'agchou', age: 27, notes:'hes da man!'}})
-    //   .success(function(data){
-    //     console.log(data);
-    //   });
-
-    // user collection name to get documents
-
 
 
 angular.module('hackathonApp')
   .service('requestServices', function($http){
 
-      // getCollection : function(collection,callback){
-      //   $http.get('/api/database/:collection')
-      //     .success(function(data){
-      //       callback(data);
-      //     });
-      // },
-      // postCollections : function(callback) {
-      //   $http.post('/api/database/533f4e406ffe158c16d175d4');
-      // }
       this.getListOfCollections = function(callback) {
         var self = this;
-        $http.get('/API/database/super')
+        $http.get('/API/database/testing')
           .success(function(data) {
             var data = _.pluck(data,'name');
             var data = _.map(data, function(element) {
@@ -34,58 +17,47 @@ angular.module('hackathonApp')
             var collections = _.filter(data, function(element) {
               return element !== "system";
             })
-            self.collections = collections;
-            callback();
+            callback(collections);
           })
       };
       this.getDocuments = function(collection, callback) {
         var self = this;
-        $http.get('/api/database/super/collection/' + collection)
+        $http.get('/api/database/testing/collection/' + collection)
           .success(function(data) {
             callback(data);
           });
       };
+      this.updateDocument = function(document) {
+        var id = document._id;
+        var doc = _.omit(document, '_id');
+        console.log(doc);
+        $http.put('/api/database/testing/collection/messages/id/' + id ,doc)
+        .success(function(data) {
+          console.log('document successfully updated: ',data)
+        })
+      };
   });
+
 
 angular.module('hackathonApp')
   .controller('ManagerCtrl', function ($scope,requestServices) {
 
-
-    requestServices.getListOfCollections(function() {
-      $scope.collections = requestServices.collections;
+    $scope.temp = {};
+    requestServices.getListOfCollections(function(collections) {
+      $scope.collections = collections;
     });
-
-
 
     $scope.currentCollection = undefined;
     $scope.collectionData = [];
-    var dummies = {};
-    dummies['users'] = [{_id: 1, name: 'agchou', age: 27, notes:'hes da man!'},
-      {_id: 1, name: 'agchou', age: 27, notes:'hes da man!'},
-      {_id: 1, name: 'agchou', age: 27, notes:'hes da man!'},
-      {_id: 1, name: 'agchou', age: 27, notes:'hes da man!'},
-      {_id: 1, name: 'agchou', age: 27, notes:'hes da man!'},
-      {_id: 1, name: 'agchou', age: 27, notes:'hes da man!'},
-    ];
-    dummies['messages'] = [{_id: 1, awesomeness: 'charles', age: 27, notes:'hes da man!'},
-      {_id: 1, awesomeness: 'charles', age: 27, notes:'hes da shit!'},
-      {_id: 1, awesomeness: 'charles', age: 27, notes:'hes da shit!'},
-      {_id: 1, awesomeness: 'charles', age: 27, notes:'hes da shit!'},
-      {_id: 1, awesomeness: 'charles', age: 27, notes:'hes da shit!'},
-      {_id: 1, awesomeness: 'charles', age: 27, notes:'hes da shit!'},
-    ];
-
-
 
     $scope.displayCollection = function(collection) {
       // requestServices.getCollections(function(data){
       //   $scope.collectionData = data;
       // })
-      console.log($scope.collections);
       $scope.currentCollection = collection;
       requestServices.getDocuments(collection, function(documents) {
         $scope.collectionData = documents;
-        $scope.collectionKeys = Object.keys($scope.collectionData[0]);
+        $scope.collectionKeys = Object.keys($scope.collectionData[0]).sort();
       });
     };
 
@@ -111,11 +83,16 @@ angular.module('hackathonApp')
       // switch to another collection
     };
 
-    $scope.saveChanges = function() {
-      // Sends entire collection back
-      // POST /api/database/:id/:collection
-    };
 
+    $scope.saveDocument = function(document, key) {
+      $scope.temp[key] = document;
+    };
+    $scope.updateDocument = function() {
+      requestServices.updateDocument(document);
+    }
+    $scope.show = function(collection) {
+      console.log(collection);
+    }
 
 
   });
