36e44585447f0c2a9bf794932c4b7a029d3dd22d
Charles Tai <cwtai86@gmail.com>
2014-04-11 17:19:47 -0700

    Changed requestServices to factory from service

--- app/scripts/controllers/manager.js
+++ app/scripts/controllers/manager.js
@@ -4,52 +4,55 @@
 
 
 angular.module('hackathonApp')
-  .service('requestServices', function($http){
-
-      this.getListOfCollections = function(callback) {
-        var self = this;
-        $http.get('/API/database/testing')
-          .success(function(data) {
-            var data = _.pluck(data,'name');
-            var data = _.map(data, function(element) {
-              return element.split('.')[1];
+  .factory('requestServices', function($http) {
+      var service = {
+        currentCollection:'',
+        getListOfCollections: function(callback) {
+          $http.get('/API/database/' + 'testing')
+            .success(function(data) {
+              var data = _.pluck(data,'name');
+              var data = _.map(data, function(element) {
+                return element.split('.')[1];
+              })
+              var collections = _.filter(data, function(element) {
+                return element !== "system";
+              })
+              callback(collections);
             })
-            var collections = _.filter(data, function(element) {
-              return element !== "system";
-            })
-            callback(collections);
-          })
-      };
-      this.getDocuments = function(collection, callback) {
-        $http.get('/api/database/testing/collection/' + collection)
-          .success(function(data) {
-            callback(data);
-          });
-      };
-      this.updateDocument = function(doc,id,callback) {
-        return $http.put('/api/database/testing/collection/messages/id/' + id ,doc)
-        .success(function() {
-          console.log('document successfully updated');
-        })
-        .error(function() {
-          console.log('document failed to update');
-        });
-      };
-      this.deleteDocument = function(id) {
-        return $http.delete('/api/database/testing/collection/messages/id/' + id)
+        },
+        getDocuments: function(callback) {
+          console.log(this.currentCollection);
+          $http.get('/api/database/testing/collection/' + this.currentCollection)
+            .success(function(data) {
+              callback(data);
+            });
+        },
+        updateDocument: function(doc,id,callback) {
+          return $http.put('/api/database/testing/collection/messages/id/' + id ,doc)
           .success(function() {
-            console.log('document successfully deleted');
+            console.log('document successfully updated');
           })
           .error(function() {
-            console.log('document could not be deleted');
-          })
+            console.log('document failed to update');
+          });
+        },
+        deleteDocument: function(id) {
+          return $http.delete('/api/database/testing/collection/messages/id/' + id)
+            .success(function() {
+              console.log('document successfully deleted');
+            })
+            .error(function() {
+              console.log('document could not be deleted');
+            })
+        },
+        createDocument: function(doc) {
+          $http.post('/api/database/testing/collection/messages/id',doc)
+            .success(function() {
+              console.log('document successfully created!');
+            })
+        }
       }
-      this.createDocument = function(doc) {
-        $http.post('/api/database/testing/collection/messages/id',doc)
-          .success(function() {
-            console.log('document successfully created!');
-          })
-      }
+      return service;
   });
 
 
@@ -57,15 +60,20 @@ angular.module('hackathonApp')
   .controller('ManagerCtrl', function ($scope,requestServices) {
 
     $scope.temp = {};
+    requestServices.getListOfCollections(function(collections) {
+      $scope.collections = collections;
+      console.log($scope.collections);
+    });
+
+
+
     $scope.currentCollection = undefined;
     $scope.collectionData = [];
 
-    requestServices.getListOfCollections(function(collections) {
-      $scope.collections = collections;
-    });
     $scope.displayCollection = function(collection) {
       $scope.currentCollection = collection;
-      requestServices.getDocuments(collection, function(documents) {
+      requestServices.currentCollection = collection;
+      requestServices.getDocuments(function(documents) {
         $scope.collectionData = documents;
         $scope.collectionKeys = Object.keys($scope.collectionData[0]).sort();
       });
@@ -96,7 +104,5 @@ angular.module('hackathonApp')
       $scope.temp = {}; // resets temp for next document
       return requestServices.updateDocument(doc,id);
     }
-
   });
-
 
