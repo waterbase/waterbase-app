3a965491b461f90a3093c743a0cd33fd86c2585c
RitzyTunas <7riste@gmail.com>
2014-04-12 19:00:19 -0700

    move dashboard services into server.js

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -1,31 +1,13 @@
 'use strict';
 
 angular.module('waterbaseApp')
-  .factory('ServerService', function($http){
-    var service = {
-      startServer: function (server) {
-        return $http.post('api/servers/' + server._id + '/start');
-      },
-      stopServer: function (server) {
-        return $http.post('api/servers/' + server._id + '/stop');
-      },
-      deleteServer: function (server) {
-        var confirmDelete = confirm('Are you sure you wish to delete ' + server.name + '?');
-        if (confirmDelete) {
-          return $http.delete('api/servers/' + server._id);
-        }
-      },
-    };
-    return service;
-  })
   .controller('DashboardCtrl', function($scope, $http, $route, ServerService) {
     
-    $scope.getServers = function () {
-      $http.get('/api/servers').success(function(servers) {
+    $scope.getServers = function() {
+      ServerService.getServers().success(function(servers) {
         $scope.servers = servers;
-
         $scope.servers.forEach(function (server) {
-          Object.keys(server.resources).forEach(function (resource) {
+          Object.keys(server.resources).forEach(function(resource) {
             server.resources[resource].description = '/' + resource + ': ';
             server.resources[resource].description += JSON.stringify(server.resources[resource].attributes);
           });
@@ -37,7 +19,7 @@ angular.module('waterbaseApp')
 
     // ServerService
     $scope.deleteServer = function(server){
-      ServerService.deleteServer(server).success(function () {
+      ServerService.deleteServer(server).success(function() {
         // remove server from client view
         $scope.servers.splice($scope.servers.indexOf(server), 1);
 
